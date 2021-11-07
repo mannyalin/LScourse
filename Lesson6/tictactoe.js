62add59943cfb1ec5fdbd65fddf22c14fcbd3588
Manuel Alingog <manuelalingog@gmail.com>
2021-11-06 20:03:20 -0700

    offensive computer

--- Lesson6/tictactoe.js
+++ Lesson6/tictactoe.js
@@ -3,13 +3,19 @@ const readline = require('readline-sync');
 const INITIAL_MARKER = ' ';
 const HUMAN_MARKER = 'X';
 const COMPUTER_MARKER = 'O';
+const WINSCORE = 5;
+const WINNINGLINES = [
+  [1, 2, 3], [4, 5, 6,], [7, 8, 9],
+  [1, 4, 7], [2, 5, 8], [3, 6, 9],
+  [1, 5, 9], [3, 5, 7]
+];
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
 function displayBoard(board){
-console.clear();
+  console.clear();
 
 console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`)
 
@@ -54,7 +60,7 @@ function joinOr(arr, delimeter = ', ', lastDelimeter = 'or ') {
   } else if (arr.length === 2) {
     return `${arr[0]} ${lastDelimeter} ${arr[1]}`;
   } else {
-    emptyChoices + lastDelimeter + arr[arr.length - 1];
+    return emptyChoices + lastDelimeter + arr[arr.length - 1];
   }
 }
  
@@ -69,13 +75,53 @@ function playerChoosesSquare(board) {
      
     prompt("Sorry, that is not a valid choice.");
   }
-  board[square] = HUMAN_MARKER;
+  return board[square] = HUMAN_MARKER;
 }
 
 function computerChoosesSquare(board) {
-  let randomIndex = Math.floor(Math.random() * emptySquares.length);
-  let square = emptySquares(board)[randomIndex]
-  board[square] = COMPUTER_MARKER;
+  for (let line = 0; line < WINNINGLINES.length; line++) {
+    let [sq1, sq2, sq3] = WINNINGLINES[line]
+
+    if (
+        board[sq1] === ' ' &&
+        board[sq2] === COMPUTER_MARKER &&    
+        board[sq3] === COMPUTER_MARKER ) {
+          return board[sq1] = COMPUTER_MARKER;
+      } else if (
+        board[sq1] === COMPUTER_MARKER &&
+        board[sq2] === ' ' &&    
+        board[sq3] === COMPUTER_MARKER ) {
+          return board[sq2] = COMPUTER_MARKER;
+      } else if (
+        board[sq1] === COMPUTER_MARKER &&
+        board[sq2] === COMPUTER_MARKER &&    
+        board[sq3] === ' ' ) {
+          return board[sq3] = COMPUTER_MARKER;
+      } else if (
+        board[sq1] === ' ' &&
+        board[sq2] === HUMAN_MARKER &&    
+        board[sq3] === HUMAN_MARKER ) {
+          return board[sq1] = COMPUTER_MARKER;
+      } else if (
+        board[sq1] === HUMAN_MARKER &&
+        board[sq2] === ' ' &&   
+        board[sq3] === HUMAN_MARKER ) {
+          return board[sq2] = COMPUTER_MARKER;
+      } else if (
+        board[sq1] === HUMAN_MARKER &&
+        board[sq2] === HUMAN_MARKER &&    
+        board[sq3] === ' ') {
+          return board[sq3] = COMPUTER_MARKER;
+        }
+      }     
+    
+      if (board['5'] === ' ' ) {
+        return board['5'] = COMPUTER_MARKER;
+      } 
+        let randomIndex = Math.floor(Math.random() * emptySquares.length);
+        let square = emptySquares(board)[randomIndex]
+        return board[square] = COMPUTER_MARKER;  
+
 }
 
 function boardFull(board) {
@@ -87,14 +133,8 @@ function someoneWon(board) {
 }
 
 function detectWinner(board) {
-  let winningLines = [
-    [1, 2, 3], [4, 5, 6,], [7, 8, 9],
-    [1, 4, 7], [2, 5, 8], [3, 6, 9],
-    [1, 5, 9], [3, 5, 7]
-  ]
-
-  for (let line = 0; line < winningLines.length; line++) {
-    let [sq1, sq2, sq3] = winningLines[line]
+  for (let line = 0; line < WINNINGLINES.length; line++) {
+    let [sq1, sq2, sq3] = WINNINGLINES[line]
 
     if (
         board[sq1] === HUMAN_MARKER &&
@@ -115,32 +155,39 @@ function detectWinner(board) {
 }
 
 while (true) {
-  let board = initializeBoard();
-
+  let playerScore = 0;
+  let computerScore = 0;
+  
   while (true) {
+    let board = initializeBoard();
+    
+    while (true) {
+      displayBoard(board);
+      
+      playerChoosesSquare(board);
+      if (someoneWon(board) || boardFull(board)) break;
+
+      computerChoosesSquare(board);
+      if (someoneWon(board) || boardFull(board)) break;  
+    }
+    
     displayBoard(board);
+    
+    if (someoneWon(board)) {
+      prompt(`${detectWinner(board)} earns a point!`);
+      detectWinner(board) === 'Player' ? playerScore += 1 : computerScore +=1;
+    } else {
+      prompt("It's a tie!");
+    }
+    console.log(`Player : ${playerScore}\nComputer : ${computerScore}`)
   
-    playerChoosesSquare(board);
-    if (someoneWon(board) || boardFull(board)) break;
-  
-    computerChoosesSquare(board);
-    if (someoneWon(board) || boardFull(board)) break;  
-  }
-  
-  displayBoard(board);
-  
-  if (someoneWon(board)) {
-    prompt(`${detectWinner(board)} won!`)
-  } else {
-    prompt("It's a tie!");
+    prompt('Play again? (y or n)');
+    let answer = readline.question().toLowerCase()[0]
+    if (answer !== 'y') break;
   }
 
-  prompt('Play again? (y or n)');
-  let answer = readline.question().toLowerCase()[0]
-  if (answer !== 'y') break;
+  if(playerScore === WINSCORE || computerScore === WINSCORE) break;
 }
 
 prompt('Thanks for playing Tic Tac Toe!')
-
-
 
--- Lesson6/tttHighLevelPlan.js
+++ Lesson6/tttHighLevelPlan.js
@@ -8,3 +8,19 @@
 // 8. Ask if play again?
 // 9. If yes, repeat from step 1.
 // 10. If no, end game. Bye.
+
+
+// write a function that receives an array, an optional delimeter that defaults to a comma followed by a space, and a string that defaults to 'Or'
+
+// joinOr([1, 2, 3]);               // => "1, 2, or 3"
+// joinOr([1, 2, 3], '; ');         // => "1; 2; or 3"
+// joinOr([1, 2, 3], ', ', 'and');  // => "1, 2, and 3"
+// joinOr([]);                      // => ""
+// joinOr([5]);                     // => "5"
+// joinOr([1, 2]);                  // => "1 or 2"
+
+// Delimeter is optional and returns ',' when omitted
+// word connector is the third argument, is optional and defaults to 'or'
+// Passing an empty array returns empty string
+// Passing one element in the array, returns that one element as a string
+// Passing two elements in the array, returns 
