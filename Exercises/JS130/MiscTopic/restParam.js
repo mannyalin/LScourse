97c9830ce33a6fc75dce33f592e0c8de07a93050
Manuel Alingog <manuelalingog@gmail.com>
2022-06-29 23:06:24 -0700

    complete misc topics

---
+++ Exercises/JS130/MiscTopic/restParam.js
@@ -0,0 +1,18 @@
+// How can we refactor the function definition below (without changing the function invocation) so that we don't need to use the arguments object?
+
+// function sum() {
+//   values = Array.prototype.slice.call(arguments);
+
+//   return values.reduce(function(a, b) {
+//     return a + b;
+//   });
+// }
+
+function sum(...args) {
+  values = args
+
+  return values.reduce(function(a, b) {
+    return a + b;
+  });
+}
+console.log(sum(1, 4, 5, 6)); // 16
